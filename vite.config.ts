d14cf4a0e16ba8cf72c985b0a658d6433e369817
divyam desai <divyamdesai.co22d1@scet.ac.in>
2025-09-10 13:01:08 +0000

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_URL || '/AI-No-code',
+  base: "/" || '/AI-No-code',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
